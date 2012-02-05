c3ba7e44ffecf085d889f52165c07a3ca767eb60
yukiasu <yukiasu@LIVE.JP>
2012-02-06 02:39:37 +0900

    表示するViewをボタンで切り替えれるようにした。

--- LockScreenOverlay/src/jp/dip/sugarhouse/lockscreenoverlay/OverlayService.java
+++ LockScreenOverlay/src/jp/dip/sugarhouse/lockscreenoverlay/OverlayService.java
@@ -4,11 +4,12 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.os.Binder;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 /**
  * Activityに関係無くViewを表示し続けるためのサービス。<br>
@@ -17,25 +18,29 @@ import android.view.WindowManager;
  * @author YukiAsu
  */
 public class OverlayService extends Service {
-	
+
+	/** {@link Binder}。{@link #onBind(Intent)}の戻り値が{@link IBinder}なのでIBinderになってる。中身は{@link Binder}のextends */
+	public final IBinder mBinder = new OverlayServiceBinder();
+
 	/** ViewをActivityに関係なく画面に放り込むためのWindowManager */
 	private WindowManager wm;
 	/** じっさいに画面に表示されるView */
-	private View view;
+	private FrameLayout view;
 	/** Viewのパラメータ。これによってロック画面の上に鎮座させる */
 	private WindowManager.LayoutParams params;
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+		return mBinder;
 	}
 	
 	@Override
 	public void onCreate(){
 		// WindowManagerを取得して、xmlから簡素なViewを生成して、Viewの属性も準備しておく
 		wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        LayoutInflater layoutInflater = LayoutInflater.from(this);
-        view = layoutInflater.inflate(R.layout.overlay, null);
+		LayoutInflater layoutInflater = LayoutInflater.from(this);
+		this.view = new FrameLayout(getApplicationContext());
+		((FrameLayout)this.view).addView(layoutInflater.inflate(R.layout.overlay, null));
         params = new WindowManager.LayoutParams(
         	    WindowManager.LayoutParams.WRAP_CONTENT,
         	    WindowManager.LayoutParams.WRAP_CONTENT,
@@ -43,6 +48,27 @@ public class OverlayService extends Service {
         	    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |		// タッチイベントを拾わない。ロック画面を邪魔しない。
         	    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,			// フォーカスされない。ハードキーで選ぼうとかしても、無理。
         	    PixelFormat.TRANSLUCENT); 						// ウィンドウの透明化
+
+    	wm.addView(view, params);
+	}
+	
+	/**
+	 * 表示するViewのresourceIDを指定する
+	 * @param resource 表示させたいresource
+	 */
+	public void setView(int resource){
+		view.removeAllViews();
+		LayoutInflater layoutInflater = LayoutInflater.from(this);
+        this.view.addView(layoutInflater.inflate(R.layout.overlay, null));
+	}
+	
+	/**
+	 * 表示するViewを直接指定する
+	 * @param view 表示させたいView
+	 */
+	public void setView(View view){
+		this.view.removeAllViews();
+		this.view.addView(view);
 	}
 	
 	/**
@@ -50,8 +76,6 @@ public class OverlayService extends Service {
 	 */
 	@Override
 	public void onStart(Intent intent, int startId) {
-		Log.d("overlay","OverlayService start!");
-    	wm.addView(view, params);    
 	}
 	
 	/**
@@ -60,5 +84,16 @@ public class OverlayService extends Service {
 	@Override
 	public void onDestroy() {
 		wm.removeView(view);
+	}
+
+	/**
+	 * bindするための橋渡し、{@link Binder}<br>
+	 * コレを介して自分自身を渡してあげる事で、Serviceのpublicメソッドが使用できるようになります。
+	 */
+	public class OverlayServiceBinder extends Binder {
+		OverlayService getService() {
+			// クライアントがpublicメソッドを呼び出せるように、自分自身のインスタンスを返す。
+			return OverlayService.this;
+		}
 	}
 }
--- LockScreenOverlay/src/jp/dip/sugarhouse/lockscreenoverlay/Setting.java
+++ LockScreenOverlay/src/jp/dip/sugarhouse/lockscreenoverlay/Setting.java
@@ -1,17 +1,32 @@
 package jp.dip.sugarhouse.lockscreenoverlay;
 
+import jp.dip.sugarhouse.lockscreenoverlay.OverlayService.OverlayServiceBinder;
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 /**
  * 設定画面。というよりなんのことは無くボタンがあるだけ。startを押せばserviceが立ち上がってViewを表示し、stopを押せばserviceを殺してViewには消えて頂く。
  * @author YukiAsu
  */
 public class Setting extends Activity {
+	
+	/** 表示を管理しているService。{@link ServiceConnection}によってサービス内のpublicメソッドをごりごり呼び出す */
+	private OverlayService overlayService;
+	/** いま{@link Service}はbindされてる？っていうフラグ。trueならされてる。 */
+	private boolean mBound = false;
+
 	/** Activityが生成される時に呼ばれる */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,7 @@ public class Setting extends Activity {
 			@Override
 			public void onClick(View v) {
 				startService(overlayService);
+				bindService(overlayService, mConnection, Context.BIND_AUTO_CREATE);
 			}
 		});
 
@@ -33,7 +49,58 @@ public class Setting extends Activity {
 			@Override
 			public void onClick(View v) {
 				stopService(overlayService);
+				if (mBound) {
+		            unbindService(mConnection);
+		            mBound = false;
+		        }
+			}
+		});
+		
+		((Button)findViewById(R.id.changeView1)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Setting.this.overlayService.setView(new Button(getApplicationContext()));
+			}
+		});
+		
+		((Button)findViewById(R.id.changeView2)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Setting.this.overlayService.setView(new RadioButton(getApplicationContext()));
+			}
+		});
+		
+		((Button)findViewById(R.id.changeView3)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Setting.this.overlayService.setView(new EditText(getApplicationContext()));
 			}
 		});
 	}
+
+	/**
+	 * {@link ServiceConnection}を使う事でServiceにあるpublicのメソッドをActivityからでも呼び出せます。<br>
+	 */
+	private ServiceConnection mConnection = new ServiceConnection() {
+		/**
+		 * @param className 接続されたServiceの名前とかそこら。
+		 * @param service 接続されたServiceの{@link Binder}。コレを使ってサービスのインスタンスをひっぱってきていじくる
+		 */
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// We've bound to LocalService, cast the IBinder and get LocalService instance
+			OverlayServiceBinder binder = (OverlayServiceBinder)service;
+			overlayService = binder.getService();
+			mBound = true;
+		}
+
+		/**
+		 * Serviceから切り離された時に呼ばれる。ただし、正常に{@link Setting#unbindService(ServiceConnection)}された場合は呼ばれない。<br>
+		 * これが呼ばれるのはクラッシュしたり強制終了された場合。
+		 */
+		@Override
+		public void onServiceDisconnected(ComponentName className) {
+			mBound = false;
+		}
+	};
 }
